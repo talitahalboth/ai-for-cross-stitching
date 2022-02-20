3918e0fd46844a407024e12250803a16ec108984
Talita Halboth <talitahalboth@gmail.com>
2022-02-20 18:09:44 -0300

    Update run.py

--- geneticAlgorithm/run.py
+++ geneticAlgorithm/run.py
@@ -23,21 +23,8 @@ def ga_tests():
     print('Executando Algoritmo Genético...')
     for instance in instances:
         problem = TSP(instance)
-        # historiy_list = []
-        # mean_time = 0
-        # mean_cost = 0
-        # print(problem)
-        # start_time = time.time()
         path, history = genetic_algorithm(problem)
-        # mean_time += time.time() - start_time
         cost = problem.evaluate(path)
-        # mean_cost += cost
-        # print('Custo do caminho:', cost)
-        # historiy_list.append(history)
-        # mean_history = [sum(x) for x in zip(*historiy_list)]
-        # print('Custo medio:', mean_cost/5)
-        # print('Tempo medio:', str(mean_time/5), 'segundos')
-        # plot_evolution(mean_history, problem)
 
 
 # a_star_tests()
