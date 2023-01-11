a2fd996a88c7339508ae5ff170d32f1fce0f4f6c
Talita Halboth <talitahalboth@gmail.com>
2023-01-10 22:25:50 -0300

    🐛  Fix file path (#11)
    * :bug: fix file paths
    
    * Update templatematching.py
    
    * run in linux
    
    Signed-off-by: Talita Halboth <thcf16@inf.ufpr.br>
    
    * Update findtemplateimages.py
    
    * run linux again
    
    Signed-off-by: Talita Halboth <thcf16@inf.ufpr.br>
    
    Signed-off-by: Talita Halboth <thcf16@inf.ufpr.br>
    Co-authored-by: Talita Halboth <19466053+talitahalboth@users.noreply.github.com>
    Co-authored-by: Talita Halboth <thcf16@inf.ufpr.br>

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from src.logger import SingletonLogger
 from src.findtemplateimages import find_template_images
 from src.templatematching import template_matching
@@ -12,7 +14,7 @@ if __name__ == "__main__":
     parser.add_argument("-D", "--debug", help="Output debug logs",
                         action="store_true")
     # TODO: add / to end of dir name
-    parser.add_argument("-d", "--directory", help="Directory location of pattern", default="examples/starryNight/",
+    parser.add_argument("-d", "--directory", help="Directory location of pattern", default="examples/heart/",
                         action="store")
     parser.add_argument("-f", "--file", help="File name of pattern (default: img.png). "
                                              "The file must preferably have dimensions over 1500px", default="img.png",
@@ -25,15 +27,28 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     logger = SingletonLogger(args.verbose, args.debug)
-    templates_directory = str(args.directory) + "templates/"
+    directory = Path(args.directory)
+    templates_directory = directory / "templates"
 
+    if not os.path.isdir(args.directory):
+        logger.log("Directory does not exist", "ERROR")
+        exit(-1)
+
+    if not os.path.isfile(directory / args.file):
+        logger.log("File does not exist", "ERROR")
+        exit(-1)
     # todo: set output directory
-    if not os.path.isdir(args.directory + "/paths/"):
-        os.makedirs(args.directory + "/paths/")
-
+    output_directory = Path(directory / "paths")
+    if args.output is None and not os.path.isdir(directory / "paths"):
+        os.makedirs(directory / "paths")
+    if args.output is not None:
+        output_directory = Path(args.output)
+        if not os.path.isdir(output_directory):
+            os.makedirs(output_directory)
+    # template_matching(args.directory, templates_directory, args.file)
     if args.templates is not None:
         templates_directory = args.templates
-        template_matching(args.directory, templates_directory, args.file)
+        template_matching(directory, templates_directory, args.file, output_directory)
     else:
-        find_template_images(args.directory, args.file)
+        find_template_images(directory, args.file, output_directory)
     # TODO: pass outputs argument
